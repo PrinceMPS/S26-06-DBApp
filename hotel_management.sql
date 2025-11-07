6a446e51c95cb3918aad450c1a3e820840fd993d
PrinceMPS <prinmat88@gmail.com>
2025-11-07 08:22:00 +0800

    change to follow 3nf

--- hotel_management.sql
+++ hotel_management.sql
@@ -8,18 +8,25 @@ CREATE TABLE guest(
     contact_number INT NOT NULL,
     email_address VARCHAR(35) NOT NULL,
     nationality VARCHAR(20) NOT NULL,
-    age INT NOT NULL,
     PRIMARY KEY (guest_id)
 );
 
+CREATE TABLE RoomType (
+    room_type_id INT PRIMARY KEY AUTO_INCREMENT,
+    type_name VARCHAR(50) NOT NULL,
+    rate_per_type DECIMAL(10,2) NOT NULL,
+    capacity INT NOT NULL
+);
+
 CREATE TABLE room(
-    room_id INT NOT NULL,
+    room_id INT PRIMARY KEY AUTO_INCREMENT,
     room_type INT NOT NULL,
-    availability ENUM('Booked', 'Vacant') DEFAULT 'Vacant' NOT NULL,
-    housekeeping ENUM('For Cleaning', 'Under Maintainance', 'Ready') DEFAULT 'Ready' NOT NULL,
-    capacity INT NOT NULL,
-    rate INT NOT NULL,
-    PRIMARY KEY (room_id)
+    availability_status ENUM('Booked', 'Vacant') DEFAULT 'Vacant' NOT NULL,
+    housekeeping_status ENUM('For Cleaning', 'Under Maintainance', 'Ready') DEFAULT 'Ready' NOT NULL,
+    room_type_id INT,
+   
+   -- Foreign Key
+    FOREIGN KEY (room_type_id) REFERENCES roomtype(room_type_id)
 );
 
 CREATE TABLE employee(
@@ -27,39 +34,39 @@ CREATE TABLE employee(
 	first_name VARCHAR(20) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
     position VARCHAR(10) NOT NULL,
-    shift INT NOT NULL,
+    shift ENUM('Morning', 'Afternoon', 'Night') NOT NULL,
     emp_status ENUM('Active', 'Leave-vacation', 'Leave-sick', 'Leave-maternty') DEFAULT 'Active' NOT NULL,
     PRIMARY KEY (employee_id)
 );
 
-CREATE TABLE housekeeping(
-    housekeeping_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE housekeeping_item(
+    housekeeping_item_id INT NOT NULL AUTO_INCREMENT,
 	item_name VARCHAR(20) NOT NULL,
     quantity INT NOT NULL,
-    cost INT NOT NULL,
+    cost_per_unit INT NOT NULL,
     stock INT NOT NULL,
-    max VARCHAR(20) NOT NULL,
-    PRIMARY KEY (housekeeping_id)
+	max_stock_storage VARCHAR(20) NOT NULL,
+    PRIMARY KEY (housekeeping_item_id)
 );
 
 CREATE TABLE booking (
     booking_id INT NOT NULL AUTO_INCREMENT,
     guest_id INT NOT NULL,
     room_id INT NOT NULL,
-    employee_id INT NOT NULL,
     booking_date DATE NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
     
     PRIMARY KEY (booking_id),
     
     -- Foreign Keys
     FOREIGN KEY (guest_id) REFERENCES guest(guest_id),
-    FOREIGN KEY (room_id) REFERENCES room(room_id),
-    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
+    FOREIGN KEY (room_id) REFERENCES room(room_id)
+    
 );
 
 CREATE TABLE payment (
     payment_id INT NOT NULL AUTO_INCREMENT,
-    guest_id INT NOT NULL,
     booking_id INT NOT NULL,
     amount_paid DECIMAL(10,2) NOT NULL,
     payment_method VARCHAR(20) NOT NULL,
@@ -68,29 +75,29 @@ CREATE TABLE payment (
     PRIMARY KEY (payment_id),
     
     -- Foreign Keys
-    FOREIGN KEY (guest_id) REFERENCES guest(guest_id),
     FOREIGN KEY (booking_id) REFERENCES booking(booking_id)
 );
 
-CREATE TABLE check_in_out (
+CREATE TABLE GuestStay(
     transaction_id INT NOT NULL AUTO_INCREMENT,
     booking_id INT NOT NULL,
-    check_in_date TIMESTAMP,
-    check_out_date TIMESTAMP,
-    room_status VARCHAR(15) NOT NULL,   
+    employee_id INT NOT NULL,
+    check_in_time_date DATETIME,
+    check_out_time_date DATETIME,
     remarks VARCHAR(100),
 
     PRIMARY KEY (transaction_id),
     
     -- Foreign Keys
-    FOREIGN KEY (booking_id) REFERENCES booking(booking_id)
-	
+    FOREIGN KEY (booking_id) REFERENCES booking(booking_id),
+    FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
 );
 
-CREATE TABLE housekeeping_issuance (
+
+
+CREATE TABLE housekeeping_item_issuance (
     issuance_id INT NOT NULL AUTO_INCREMENT,
-    housekeeping_id INT NOT NULL,
-    room_id INT NOT NULL,
+	housekeeping_item_id  INT NOT NULL,
     employee_id INT NOT NULL,
     quantity_issued INT NOT NULL,
     date_issued DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -99,7 +106,7 @@ CREATE TABLE housekeeping_issuance (
     PRIMARY KEY (issuance_id),
 
     -- Foreign Keys
-    FOREIGN KEY (housekeeping_id) REFERENCES housekeeping(housekeeping_id),
-    FOREIGN KEY (room_id) REFERENCES room(room_id),
+    FOREIGN KEY (housekeeping_item_id ) REFERENCES housekeeping_item(housekeeping_item_id ),
     FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
 );
+
