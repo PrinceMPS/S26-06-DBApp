66552a0bcc2684416f195e582bfc41064d680a0a
PrinceMPS <prinmat88@gmail.com>
2025-11-12 23:43:22 +0800

    add get post for booking

--- controllers/bookings_controller.py
+++ controllers/bookings_controller.py
@@ -1,10 +1,25 @@
-from flask import Blueprint, render_template
-from models.bookings_model import get_all_bookings
+from flask import Blueprint, render_template, request, redirect, url_for
+from models.bookings_model import get_all_bookings, add_booking
 
 bookings_bp = Blueprint('bookings', __name__, template_folder='../templates')
 
-@bookings_bp.route('/bookings')
+
+@bookings_bp.route('/bookings', methods=['GET', 'POST'])
 def bookings_list():
+    if request.method == 'POST':
+        # Read form data
+        guest_id = request.form.get('guest_id')
+        room_id = request.form.get('room_id')
+        start_date = request.form.get('start_date')
+        end_date = request.form.get('end_date')
+
+        # Insert into database
+        add_booking(guest_id, room_id, start_date, end_date)
+
+        # Redirect to avoid form resubmission
+        return redirect(url_for('bookings.bookings_list'))
+
+    # Handle GET: display all bookings
     bookings = get_all_bookings()
 
     # Convert datetime/date fields to string
