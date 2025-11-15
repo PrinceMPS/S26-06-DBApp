673bc49e29f771d6ec4feae67dffe6f51371914b
PrinceMPS <prinmat88@gmail.com>
2025-11-16 00:51:35 +0800

    fixed dashboard and index page

---
+++ controllers/index_controller.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+from models.index_model import get_report_cards
+
+reports_bp = Blueprint('reports', __name__, template_folder='../templates')
+
+@reports_bp.route('/reports')
+def reports_page():
+    reports = get_report_cards()
+    return render_template('reports/index.html', reports=reports)
