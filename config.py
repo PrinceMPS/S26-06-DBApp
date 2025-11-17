411d920a222362ebf52a07ff0a768fbf725d1fda
PrinceMPS <prinmat88@gmail.com>
2025-11-18 02:36:33 +0800

    test

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "your_username",
+    "password": "your_password",
+    "database": "hotel_management"
+}
+
+# Add this simple secret key
+SECRET_KEY = 'your-secret-key-here-12345'
